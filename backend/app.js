55ad27ae82d84a96335e19f20d96f60b12f31a1b
DESKTOP-SLN3L38\Dr Lekvárossy Lekvár <gmiki91@gmail.com>
2020-09-28 22:37:13 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 app=express();
 
-app.use((req,res,next)=>{
-    
-    next();
-});
 
 app.use((req,res,next)=>{
-    res.send(" ...and welcome!"); 
+    res.send(" Welcome!"); 
 });
 
 module.exports=app;
