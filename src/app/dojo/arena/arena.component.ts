e9973c17c17d86428494bac3ab146aec8aeb4fd3
DESKTOP-SLN3L38\Dr Lekvárossy Lekvár <gmiki91@gmail.com>
2020-11-13 18:22:21 +0100

    master list clickable or not

--- src/app/dojo/arena/arena.component.ts
+++ src/app/dojo/arena/arena.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Master } from '../master.model';
-import {first} from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 import { ArenaService } from './arena.service';
+import { AuthService } from 'src/app/auth/auth.service';
 @Component({
   selector: 'app-arena',
   templateUrl: './arena.component.html',
   styleUrls: ['./arena.component.css']
 })
 export class ArenaComponent implements OnInit {
-  masters :Master[];
+  masters: Master[];
   selectedMaster: Master;
 
-  constructor(private arenaService: ArenaService) { }
+  constructor(private arenaService: ArenaService, private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.arenaService.getMastersByLevel().pipe(first()).subscribe(masters=>this.masters=masters);
+    this.arenaService.getMastersByLevel().pipe(first()).subscribe(masters => this.masters = masters);
   }
+
   onSelect(master: Master): void {
     this.selectedMaster = master;
+  }
+  
+  isMasterAvailable(master: Master): boolean {
+
+    return this.authService.user.rank <= master.rank;
+    
   }
 }
