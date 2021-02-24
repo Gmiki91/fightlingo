b52b318dfa2f5e31f29924645e44c9ce00e26f02
DESKTOP-SLN3L38\Dr Lekvárossy Lekvár <gmiki91@gmail.com>
2021-02-24 15:53:37 +0100

    no components for districts, all packed in city

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Sentence } from '../models/sentence.model';
+import { Event } from 'src/app/models/event.model';
+import { EventHandler } from '../services/event-handler.service';
+import { QuizService } from '../services/quiz.service';
+import { District } from './district.enum';
+import Typewriter from 't-writer.js'
 
 @Component({
   selector: 'app-city',
@@ -6,11 +14,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent implements OnInit {
-  constructor() { }
+
+  district: District;
+  overdueSentences$: Observable<Sentence[]>;
+  startQuiz: boolean;
+  events: Event[] = [];
+  background:string = "townmap";
+
+  constructor(private eventHandler: EventHandler, private quizService: QuizService) { }
 
   ngOnInit(): void {
-    
-  }
- 
 
+  }
+
+  setDistrict(district: string) {
+    switch (district) {
+      case "outside":
+        this.district = District.OUTSIDE;
+        this.background="outside";
+        break;
+      case "wall":
+        this.district = District.WALL;
+        this.background="wall";
+        break;
+      case "suburb":
+        this.district = District.SUBURB;
+        this.background="suburb";
+        break;
+      case "downtown":
+        this.district = District.DOWNTOWN;
+        this.background="downtown";
+        break;
+      case "palace":
+        this.district = District.PALACE;
+        this.background="palace";
+        break;
+      case "docks":
+        this.district = District.DOCKS;
+        this.background="docks";
+        break;
+    }
+
+    this.init();
+  }
+
+  private init() {
+    document.querySelector('.tw').innerHTML="";
+    this.startQuiz = false;
+    this.events = this.eventHandler.getActiveEvents().filter(event => { return event.district === this.district });
+    if (this.events.length > 0) {
+      //van event
+      this.startEvent(this.events[0]);
+    }
+  }
+
+  private startEvent(event: Event): void {
+    
+    const text = this.eventHandler.getDialogForEvent(event.id);
+    const target = document.querySelector('.tw')
+    const writer = new Typewriter(target, {
+      loop: false,
+      typeColor: 'blue'
+    })
+    writer
+      .type(text)
+      .rest(250)
+      .start()
+
+    this.startQuiz = true;
+    const amount = event.overdue;
+    this.overdueSentences$ = this.quizService.getOverdueList()
+      .pipe(
+        map(sentences => { return sentences.slice(0, amount) }))
+  }
 }
